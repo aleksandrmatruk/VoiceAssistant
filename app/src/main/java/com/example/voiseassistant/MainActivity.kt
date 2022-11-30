f90bed6131d205c52028547f0d82a9366a264a55
Aleksandr Matruk <matruk.aleksandr@gmail.com>
2022-11-30 16:12:18 +0300

    - добавлен голос(вопрос, ответ)

--- app/src/main/java/com/example/voiseassistant/MainActivity.kt
+++ app/src/main/java/com/example/voiseassistant/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.voiseassistant
 
-import android.icu.text.CaseMap.Title
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.ListView
 import android.widget.ProgressBar
 import android.widget.SimpleAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.wolfram.alpha.WAEngine
+import com.wolfram.alpha.WAPlainText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,36 +34,40 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var progressBar: ProgressBar
 
-    val pods = mutableListOf<HashMap<String,String>>(
-        HashMap<String,String>().apply {
-            put("Title", "Title 1")
-            put("Content", "Content 1")
-        },
-        HashMap<String,String>().apply {
-            put("Title", "Title 2")
-            put("Content", "Content 2")
-        },
-        HashMap<String,String>().apply {
-            put("Title", "Title 3")
-            put("Content", "Content 3")
-        },
-        HashMap<String,String>().apply {
-            put("Title", "Title 4")
-            put("Content", "Content 4")
-        }
-    )
+    lateinit var waEngine: WAEngine
+
+    val pods = mutableListOf<HashMap<String, String>>()
+
+    lateinit var textToSpeech: TextToSpeech
+
+    var isTtsReady: Boolean = false
+
+    val VOICE_RECOGNITION_REQUEST_CODE: Int = 1987
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+        initWolframEngine()
+        initTts()
+
     }
 
-    fun initViews(){
+    fun initViews() {
         val toolbar: MaterialToolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        requestInput= findViewById(R.id.text_input_edit)
+        requestInput = findViewById(R.id.text_input_edit)
+        requestInput.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
+
+                val question = requestInput.text.toString()
+                askWolfram(question)
+            }
+            return@setOnEditorActionListener false
+        }
 
         val podsList: ListView = findViewById(R.id.pods_list)
         podsAdapter = SimpleAdapter(
@@ -61,10 +78,24 @@ class MainActivity : AppCompatActivity() {
             intArrayOf(R.id.title, R.id.content)
         )
         podsList.adapter = podsAdapter
+        podsList.setOnItemClickListener { parent, view, position, id ->
+            if (isTtsReady) {
+                val title = pods[position]["Title"]
+                val content = pods[position]["Content"]
+                textToSpeech.speak(content, TextToSpeech.QUEUE_FLUSH, null, title)
+            }
+        }
 
         val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
-        voiceInputButton.setOnClickListener{
-            Log.d("TAG", "FAB")
+        voiceInputButton.setOnClickListener {
+            pods.clear()
+            podsAdapter.notifyDataSetChanged()
+
+            if (isTtsReady) {
+                textToSpeech.stop()
+            }
+
+            showVoiceInputDialog()
         }
 
         progressBar = findViewById(R.id.progress_bar)
@@ -72,21 +103,131 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-       menuInflater.inflate(R.menu.toolbar_menu, menu)
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.action_stop -> {
-                Log.d("TAG", "action_stop")
+                if (isTtsReady) {
+                    textToSpeech.stop()
+                }
                 return true
             }
             R.id.action_clear -> {
-                Log.d("TAG", "action_clear")
+                requestInput.text?.clear()
+                pods.clear()
+                podsAdapter.notifyDataSetChanged()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun initWolframEngine() {
+        waEngine = WAEngine().apply {
+            appID = "WK3Q3Y-TLKTYQRHTR"
+            addFormat("plaintext")
+        }
+    }
+
+    fun showSnackbar(message: String) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_INDEFINITE)
+            .apply {
+                setAction(android.R.string.ok) {
+                    dismiss()
+                }
+                show()
+
+            }
+    }
+
+    fun askWolfram(request: String) {
+        progressBar.visibility = View.VISIBLE
+        CoroutineScope(Dispatchers.IO).launch {
+            val query = waEngine.createQuery().apply { input = request }
+            runCatching {
+                waEngine.performQuery(query)
+            }.onSuccess { result ->
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    if (result.isError) {
+                        showSnackbar(result.errorMessage)
+                        return@withContext
+                    }
+
+                    if (!result.isSuccess) {
+                        requestInput.error = getString(R.string.error_do_not_understand)
+                        return@withContext
+                    }
+
+                    for (pod in result.pods) {
+                        if (pod.isError) continue
+                        val content = StringBuilder()
+                        for (subpod in pod.subpods) {
+                            for (element in subpod.contents) {
+                                if (element is WAPlainText) {
+                                    content.append(element.text)
+                                }
+                            }
+
+                            podsAdapter.notifyDataSetChanged()
+                        }
+                        pods.add(0, HashMap<String, String>().apply {
+                            put("Title", pod.title)
+                            put("Content", content.toString())
+                        })
+                    }
+
+                }
+
+
+            }.onFailure { t ->
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.GONE
+                    showSnackbar(t.message ?: getString(R.string.error_something_went_wrong))
+                }
+            }
+        }
+    }
+
+    fun initTts() {
+        textToSpeech = TextToSpeech(this) { code ->
+            if (code != TextToSpeech.SUCCESS) {
+                Log.e("Tag", "TTS error code: $code")
+                showSnackbar(getString(R.string.error_tts_is_not_ready))
+            } else {
+                isTtsReady = true
+            }
+        }
+        textToSpeech.language = Locale.US
+    }
+
+    fun showVoiceInputDialog() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.request_hint))
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+        }
+
+        runCatching {
+            startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE)
+        }.onFailure { t ->
+            showSnackbar(t.message ?: getString(R.string.error_voice_recognition_unavailable))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == VOICE_RECOGNITION_REQUEST_CODE && requestCode == RESULT_OK) {
+            data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0)?.let { question ->
+                requestInput.setText(question)
+                askWolfram(question)
+            }
+        }
     }
 }
